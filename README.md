fced8934535b225dae2e8b19f1f0583842b046cf
Hellicat <jhccop@naver.com>
2023-10-15 16:20:10 +0900

    edit styles

---
+++ README.md
@@ -0,0 +1,27 @@
+# Clone - Tolv
+
+### Clone Coding !!!
+
+https://tolv.dk
+
+Developed By HTML, SCSS
+Built By Gulp
+Use Live Server (VScode Extention)
+
+#### Develope
+
+```
+npm run dev
+```
+
+#### Build
+
+```
+npm run build
+```
+
+#### Deploy
+
+```
+npm run deploy
+```
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,36 @@
 import gulp from "gulp";
-import { deleteSync } from "del";
+import {deleteSync} from "del";
 import minify from "gulp-csso";
 import gulpSass from "gulp-sass";
 import sass2 from "sass";
 import autoprefixer from "gulp-autoprefixer";
+import htmlmin from "gulp-htmlmin";
+import replace from "gulp-replace";
+import imagemin from "gulp-imagemin";
+import ghPages from "gh-pages";
 
 const sass = gulpSass(sass2);
 
 const routes = {
+  html: {src: "index.html", dest: "dest"},
   css: {
     watch: "src/scss/*",
     src: "src/scss/styles.scss",
     dest: "dest/css",
   },
+  img: {
+    watch: "src/img/*",
+    src: "src/img/*",
+    dest: "dest/img",
+  },
+};
+const homepage = () => {
+  gulp
+    .src(routes.html.src)
+    .pipe(replace("dest/css/", "css/"))
+    .pipe(replace("src/img/", "img/"))
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(routes.html.dest));
 };
 
 const styles = () =>
@@ -28,16 +46,29 @@ const styles = () =>
     .pipe(minify())
     .pipe(gulp.dest(routes.css.dest));
 
-const watch = () => {
-  gulp.watch(routes.css.watch, styles);
+const img = () => {
+  gulp.src(routes.img.src).pipe(imagemin()).pipe(gulp.dest(routes.img.dest));
 };
 
+const watch = () => {
+  gulp.watch(routes.html.src, homepage);
+  gulp.watch(routes.css.watch, styles);
+  gulp.watch(routes.img.watch, img);
+};
+const ghDeploy = async () => {
+  await ghPages.publish("dest", {branch: "gh-pages", message: "Auto Deploy"});
+};
 const clean = async () => await deleteSync(["dest/"]);
 
 const prepare = gulp.series([clean]);
 
-const assets = gulp.series([styles]);
+const assets = async () => {
+  await homepage();
+  await styles();
+  await img();
+};
 
 const live = gulp.parallel([watch]);
-
-export const dev = gulp.series([prepare, assets, live]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, live]);
+export const deploy = gulp.series([ghDeploy]);
